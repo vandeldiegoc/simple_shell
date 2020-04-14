666d94d27d942ade783a2a578d4e506a1dd582f4
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-14 21:19:55 +0000

    create _strcat.c

---
+++ _strcat.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+/**
+ * _strcat - concatenates two strings.
+ * @dest : pointer-parameter
+ * @src : pointer-parameter
+ * Return: dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+int sz;
+int i;
+
+for (sz = 0; dest[sz]; sz++)
+;
+for (i = 0; src[i]; i++)
+{
+dest[sz + i] = src[i];
+}
+dest[sz + i] = '\0';
+return (dest);
+}
