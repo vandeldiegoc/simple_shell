56cdf8ed90143b108f715fcdbe7e88b1e560b6cb
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-14 21:32:52 +0000

    create _strcmp

---
+++ _strcmp.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+/**
+ * _strcmp - compares two strings
+ * @str1: first string
+ * @str2: second string
+ * Return: 0 on success
+ */
+
+int _strcmp(char *str1, char *str2)
+{
+int i;
+
+i = 0;
+while (str1[i] == str2[i])
+{
+if (str1[i] == '\0')
+return (0);
+i++;
+}
+return (str1[i] - str2[i]);
+}
