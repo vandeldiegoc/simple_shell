2f041f27ec2295932d4b4c495cb3009b5050004b
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-14 21:40:14 +0000

    create _puts

---
+++ _puts.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+ * _puts - prints a string.
+ * @str : pointer-parameter
+ * Return: ntg.
+ */
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i]; i++)
+{
+if (str[i] == '\0')
+break;
+_putchar(str[i]);
+}
+}
