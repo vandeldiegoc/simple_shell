b322f4ea3f5763916f4002cba9f5ae1e44e29c57
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-16 15:36:18 +0000

    modified shell_status

--- shell_status.c
+++ shell_status.c
@@ -1,25 +1,28 @@
 #include "shell.h"
 /**
  * shell_status - determines if the tokens are executable or not.
- * @grid: pointer of tokens
+ * @grd: pointer of tokens
  * @path_dir: grid of dirs in the path
  * @env: enviroment vars.
+ * @line: input line
+ * @ex: exit number
+ *
  * Return: status
  */
-int shell_status(char **grid, char **path_dir, char **env)
+int shell_status(char **grd, char **path_dir, char **env, char *line, int *ex)
 {
 int flag = 0;
 
 /*Nothing comming*/
-if (grid[0] == NULL)
+if (grd[0] == NULL)
 return (1);
 /*builtins*/
-flag = shell_builtins(grid, env);
+flag = shell_builtins(grid);
 if (flag)
 return (flag);
 /*PATH commands*/
-if ((grid[0][0] != '/') && (shell_path(grid, path_dir)))
+if ((grd[0][0] != '/') && (shell_path(grd, path_dir)))
 return (1);
 /*exe commands or no valid*/
-return (shell_process(grid));
+return (shell_process(grd, env, line, path_dir, ex));
 }
