7664daede69087f5589b1043bef4c880b84ce53b
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-16 14:59:41 +0000

    modified shell_loop

--- shell_loop.c
+++ shell_loop.c
@@ -7,7 +7,17 @@
 void shell_loop(char **env)
 {
 char *line, *delim, *path_con, **grid, **path_dir;
-int status;
+int status, e;
+int *exit_status;
+
+exit_status = malloc(sizeof(int));
+if (!exit_status)
+{
+perror("lsh");
+exit(EXIT_FAILURE);
+}
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "$ ", 2);
 
 /*get the path content*/
 path_con = _getenv(env, "PATH");
@@ -16,17 +26,21 @@ delim = ":";
 path_dir = shell_token(path_con, delim);
 delim = " \t\r\a\n";
 do {
-_puts("$ ");
-line = shell_read(path_dir);
+line = shell_read(path_dir, exit_status);
 grid = shell_token(line, delim);
-status = shell_status(grid, path_dir, env);
+status = shell_status(grid, path_dir, env, line, exit_status);
 free(line);
 free(grid);
 if (status == -1)
 {
 free(path_dir);
-exit(0);
+e = *exit_status;
+free(exit_status);
+exit(e);
 }
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "$ ", 2);
 } while (status);
+free(exit_status);
 free(path_dir);
 }
