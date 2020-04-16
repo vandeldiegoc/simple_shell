7163528f4dd0f78b4d704ff32b42c56e4f81529d
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-16 15:31:49 +0000

    modified shell_read

--- shell_read.c
+++ shell_read.c
@@ -2,20 +2,22 @@
 /**
  * shell_read - get the line
  * @path_dir: path directories
+ * @exit_status: exit number
  * Return: the line
  */
-char *shell_read(char **path_dir)
+char *shell_read(char **path_dir, int *exit_status)
 {
 char *line = NULL;
 size_t sz = 0;
 int flag;
 
 flag = getline(&line, &sz, stdin);
-if (flag == -1)
+if (flag == EOF)
 {
 free(line);
 free(path_dir);
-exit(EXIT_FAILURE);
+free(exit_status);
+exit(0);
 }
 line[flag - 1] = '\0';
 return (line);
