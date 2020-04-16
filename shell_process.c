e3ecd4702771d957b89302dd59242887cb1842d2
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-16 15:27:53 +0000

    modified shell_process

--- shell_process.c
+++ shell_process.c
@@ -1,30 +1,42 @@
 #include "shell.h"
 /**
  * shell_process - create a child process and wait until it ends.
- * @grid: pointer to tokens
+ * @grd: pointer to tokens
+ * @env: env. vars
+ * @line: input line
+ * @path_dir: path dirs
+ * @e: exit number
+ *
  * Return: 1 on success or exit in errors
  */
-int shell_process(char **grid)
+int shell_process(char **grd, char **env, char *line, char **path_dir, int *e)
 {
-int status = 1;
+int status;
 pid_t child;
 
 child = fork();
 if (child == -1)
 {
 perror("hsh");
+exit(EXIT_FAILURE);
 }
 else if (child == 0)
 {
-if (execve(grid[0], grid, NULL) == -1)
+if (execve(grd[0], grd, env) == -1)
 {
 perror("hsh");
-exit(EXIT_FAILURE);
+free(line);
+free(grd);
+free(path_dir);
+free(e);
+exit(127);
 }
 }
 else
 {
 waitpid(child, &status, WUNTRACED);
+if (WIFEXITED(status))
+*e = WEXITSTATUS(status);
 }
 return (1);
 }
