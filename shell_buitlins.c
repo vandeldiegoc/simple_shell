07ab68fe8fb941b10098defe6a3df04e10527a9e
adonis7ejeda <adonis7ejeda@gmail.com>
2020-04-16 14:51:37 +0000

    modified shell_built

--- shell_buitlins.c
+++ shell_buitlins.c
@@ -2,16 +2,16 @@
 /**
  * shell_builtins - detect a builtin
  * @grid: tokens
- * @env: env. vars
+ *
  * Return: -1 for exit, 1 on success or 0 if not
  */
-int shell_builtins(char **grid, char **env)
+int shell_builtins(char **grid)
 {
 if (_strcmp(grid[0], "exit") == 0)
 return (-1);
 if (_strcmp(grid[0], "env") == 0)
 {
-puts_grid(env);
+puts_grid(environ);
 return (1);
 }
 return (0);
